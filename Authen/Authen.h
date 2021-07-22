663b605f3dd15475b602cdcb030b8bcf5e5ee6ee
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2021-07-22 11:38:39 +0700

    Init Authen project

---
+++ Authen/Authen.h
@@ -0,0 +1,18 @@
+//
+//  Authen.h
+//  Authen
+//
+//  Created by Tanakorn Phoochaliaw on 22/7/2564 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Authen.
+FOUNDATION_EXPORT double AuthenVersionNumber;
+
+//! Project version string for Authen.
+FOUNDATION_EXPORT const unsigned char AuthenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Authen/PublicHeader.h>
+
+
